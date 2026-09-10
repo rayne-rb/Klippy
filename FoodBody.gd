2601e1bc793788aac7ce05ab53490453ee70df97
Rayne Binneman <rayne.r.binneman@gmail.com>
2026-09-10 07:37:14 +0200

    Prevent feeding logic from executing while dragging

--- FoodBody.gd
+++ FoodBody.gd
@@ -13,7 +13,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _check_feeding() -> void:
-	if klippy_window == null or stats == null:
+	if klippy_window == null or stats == null or dragging:
 		return
 
 	var klippy_rect := Rect2i(klippy_window.position, klippy_window.size)
